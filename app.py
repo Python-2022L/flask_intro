d0725b7659c0ca20b8c33ead11065f2c88a2909e
JavohirJalilov <javohirjaliloff@gmail.com>
2023-03-29 09:07:26 +0500

    flask app

--- app.py
+++ app.py
@@ -4,6 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return {"ok":True}
+    return """
+    /home
+    /about
+    """
+
+@app.route("/product")
+def home():
+    return {
+        "name":"Samsung",
+        "color":"blue",
+        "ram":64
+    }
+
+@app.route('/about')
+def about():
+    return "About Page"
 
 app.run(debug=True, port='4180')
