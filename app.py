41f5884d6b9e1b95a9a15b08a8414a453c228a36
JavohirJalilov <javohirjaliloff@gmail.com>
2023-03-31 19:37:01 +0500

    form

--- app.py
+++ app.py
@@ -4,10 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "HOME"
+    html = """
+    <form action="http://127.0.0.1:8080/api/get-sum">
+    <label>a:</label><br>
+    <input type="text"  name="a" value="0"><br>
+    <label>b:</label><br>
+    <input type="text" name="b" value="0"><br><br>
+    <input type="submit" value="Submit">
+    </form>
+    """
+    return html
 
 @app.route("/api/get-sum")
 def sum():
+    print(request.form)
     args = request.args
     a = args.get('a', 0)
     b = args.get('b', 0)
